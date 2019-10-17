6e072e7d1233d6f2e712fa0d07e82bb0e268469b
Karla <karlasalamun9@gmail.com>
2019-10-17 16:48:25 +0200

    prva verzija

---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by karla on 17. 10. 2019..
+//
+
+#ifndef PROJEKT_MAIN_H
+#define PROJEKT_MAIN_H
+
+struct point {
+    double x, y, z;
+};
+
+double value ( point solution );
+
+#endif //PROJEKT_MAIN_H
